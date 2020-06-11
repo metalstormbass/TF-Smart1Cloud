3cdfaf46bd75d26ebd9b5af32b953f263893ede7
Michael Braun <michaelbraunbass@gmail.com>
2020-06-11 12:03:26 -0600

    Update security_groups.tf

--- security_groups.tf
+++ security_groups.tf
@@ -26,7 +26,7 @@ resource "azurerm_network_security_group" "cp-gw-nsg" {
 
 #VM NSG
 resource "azurerm_network_security_group" "cp-vm-nsg" {
-  depends_on=[azurerm_resource_group.smart1- cp-vm-rg]
+  depends_on=[azurerm_resource_group.smart1-cp-vm-rg]
   name = "cp-vm-nsg"
   location            = azurerm_resource_group.smart1-cp-vm-rg.location
   resource_group_name = azurerm_resource_group.smart1-cp-vm-rg.name
