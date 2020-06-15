7304ad6c49a936008d374d36330105d3e949312a
Michael Braun <michaelbraunbass@gmail.com>
2020-06-15 08:08:48 -0600

    Update gw-bootstrap.sh

--- gw-bootstrap.sh
+++ gw-bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Variables
-sicKey='${sic_key}'
-token='${token]'
+sicKey=$sic_key
+token=$token
 
 #This build the config string
 config_string='install_security_gw=true&install_ppak=true&gateway_cluster_member=false&install_security_managment=false&install_mgmt_primary=true&install_mgmt_secondary=false&download_info=true&hostname=MikeNet-cp-gw&mgmt_gui_clients_radio=any&mgmt_admin_radio=gaia_admin&ftw_sic_key='"$sicKey"''
