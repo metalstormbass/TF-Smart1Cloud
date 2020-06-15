e3665e93d57f56893abbb0cb9e71b6149d58f187
Michael Braun <michaelbraunbass@gmail.com>
2020-06-15 09:03:28 -0600

    Update gw-bootstrap.sh

--- gw-bootstrap.sh
+++ gw-bootstrap.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #Variables
-sicKey=$sic_key
-token=$token
+
+echo $sic_key >> /home/admin/t.txt
+echo $token >> /home/admin/t2.txt
 
 #This build the config string
-config_string='install_security_gw=true&install_ppak=true&gateway_cluster_member=false&install_security_managment=false&install_mgmt_primary=true&install_mgmt_secondary=false&download_info=true&hostname=MikeNet-cp-gw&mgmt_gui_clients_radio=any&mgmt_admin_radio=gaia_admin&ftw_sic_key='"$sicKey"''
+config_string='install_security_gw=true&install_ppak=true&gateway_cluster_member=false&install_security_managment=false&install_mgmt_primary=true&install_mgmt_secondary=false&download_info=true&hostname=MikeNet-cp-gw&mgmt_gui_clients_radio=any&mgmt_admin_radio=gaia_admin&ftw_sic_key='"$sic_key"''
 
 clish -c 'set user admin shell /bin/bash' -s
 
